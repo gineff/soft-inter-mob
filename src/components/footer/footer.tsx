8a7f346a6dbbe5d990b7769ac3909557f4e77574
gineff <canone@inbox.ru>
2024-06-24 16:20:52 +0300

    ft: add footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,57 @@
+import { Box, Grid, IconButton, Stack, Typography } from '@mui/material';
+import Logo from '../logo/logo';
+
+export const Footer = () => {
+  return (
+    <Box
+      sx={{
+        height: '286px',
+        padding: '100px',
+        pt: '50px',
+        backgroundColor: '#1B1B1B',
+        borderRadius: '40px 40px 0 0',
+        color: '#fff',
+        py: 4,
+      }}
+      component="footer"
+    >
+      <Grid container justifyContent="space-between" alignItems="flex-start">
+        {/* Левая колонка */}
+        <Grid item xs={6} sx={{}}>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <img src="/src/assets/logo.svg" alt="logo" width="280" />
+          </IconButton>
+          <Stack direction="row" spacing={5}>
+            <img src="/src/assets/telegram.svg" alt="telegram" width="40" />
+            <img src="/src/assets/vk.svg" alt="vkontakte" width="40" />
+            <img src="/src/assets/instagram.svg" alt="instagram" width="40" />
+            <img src="/src/assets/twitter.svg" alt="twitterX" width="40" />
+          </Stack>
+        </Grid>
+
+        {/* Правая колонка */}
+        <Grid item xs={6} textAlign="right">
+          <Stack direction="column" spacing={3} sx={{ display: 'inline-flex' }}>
+            <Box
+              sx={{ display: 'inline-flex', gap: '20px', alignItems: 'center' }}
+            >
+              <img src="/src/assets/email.png" alt="email" width="32" />
+              <Typography>SOFTINTERMOB@techspaceapp.com</Typography>
+            </Box>
+            <Box
+              sx={{ display: 'inline-flex', gap: '20px', alignItems: 'center' }}
+            >
+              <img src="/src/assets/phone.png" alt="phone" width="32" />
+              <Typography>+7 (999) 999-77-77</Typography>
+            </Box>
+            <Box sx={{ display: 'inline-flex', alignItems: 'center' }}>
+              <img src="/src/assets/geo.png" alt="location" width="32" />
+            </Box>
+          </Stack>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Footer;
--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -7,24 +7,31 @@ import { Technologies } from '@/pages/technologies';
 import { Partners } from '@/pages/partners';
 import { Blog } from '@/pages/blog';
 import { Faq } from '@/pages/faq';
+import { SignUp } from '@/pages/sign-up';
+import { Footer } from '@/components/footer';
 
 const Landing = () => (
-  <Container
-    sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '150px',
-    }}
-  >
-    <Main />
-    <Portfolio />
-    <About />
-    <Vacation />
-    <Technologies />
-    <Blog />
-    <Partners />
-    <Faq />
-  </Container>
+  <>
+    <Container
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '150px',
+        pb: '150px',
+      }}
+    >
+      <Main />
+      <Portfolio />
+      <About />
+      <Vacation />
+      <Technologies />
+      <Blog />
+      <Partners />
+      <Faq />
+      <SignUp />
+    </Container>
+    <Footer />
+  </>
 );
 
 export default Landing;

0645afce4211d5d7390eb8010d39024d75da620c
gineff <canone@inbox.ru>
2024-07-18 08:43:18 +0300

    fix partners width

--- src/pages/partners/partners.tsx
+++ src/pages/partners/partners.tsx
@@ -46,9 +46,6 @@ export const Partners = () => {
         {technologiesList.map(({ title, icon }) => (
           <Box
             key={title}
-            sx={{
-              width: isDesktop ? 'auto' : 'calc(33% - 79px)',
-            }}
           >
             <Card
               sx={{
